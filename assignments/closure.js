51ba89da7c0a466a412098eebdd7a8f20a6e6bf2
Binish Ali <binish83@gmail.com>
2019-05-14 10:54:36 -0400

    Pull Request-Done

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,7 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
-
+Im trying to write a closure lol
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
